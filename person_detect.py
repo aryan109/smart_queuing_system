10f947dc97b2468b21b6253a7d671ba5bd87bdb5
Aryan Pegwar <aryan.1rn17cs019@gmail.com>
2020-05-05 04:58:01 +0000

    Fix typo

--- person_detect.py
+++ person_detect.py
@@ -72,7 +72,7 @@ class PersonDetect:
                 break
             else:
                 time.sleep(1)
-        result = self.exec_network.requests[0].outputs[self.output_blob]
+        result = self.net.requests[0].outputs[self.output_blob]
         all_coords = []
         
         for box in result[0][0]:
